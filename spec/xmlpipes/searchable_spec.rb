33035853325bc3cc59fc2db31ddc8b36746c65e6
Denis Lifanov <inadsence@gmail.com>
2010-10-26 06:19:32 +0400

    [specs] XMLPipes::Searchable

--- spec/xmlpipes/searchable_spec.rb
+++ spec/xmlpipes/searchable_spec.rb
@@ -1,4 +1,25 @@
 require File.expand_path File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe XMLPipes::Searchable do
+
+  before(:each) do
+    @class = Class.new { extend XMLPipes::Searchable }
+  end
+
+  it 'provides the #search method' do
+    @class.should respond_to(:search)
+  end
+
+  describe '#search' do
+
+    it 'returns a new instance of the XMLPipes::Search' do
+      @class.search('Misaki').should be_an_instance_of XMLPipes::Search
+    end
+
+    it 'passes proper class to the XMLPipes::Search.new' do
+      @class.search('Misaki').send(:classes).should == [@class]
+    end
+
+  end
+
 end
