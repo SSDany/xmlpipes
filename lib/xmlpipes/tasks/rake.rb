5afdc892ed06be4391e845a52c8fb220e6069516
Denis Lifanov <inadsence@gmail.com>
2010-11-05 14:53:09 +0300

    [rake] xmlpipes:start, xmlpipes:stop

--- lib/xmlpipes/tasks/rake.rb
+++ lib/xmlpipes/tasks/rake.rb
@@ -2,14 +2,42 @@ namespace :xmlpipes do
 
   desc "Generate the Sphinx configuration."
   task :configure => :prepare do
-    config = XMLPipes::Configuration.instance
-    FileUtils.mkdir_p File.dirname(config.config_file)
-    puts "Generating Configuration to #{config.config_file}"
-    config.build
+    FileUtils.mkdir_p File.dirname(@config.config_file)
+    FileUtils.mkdir_p File.dirname(@config.searchd.log)
+    FileUtils.mkdir_p File.dirname(@config.searchd.pid_file)
+    FileUtils.mkdir_p @config.searchd_file_path
+    FileUtils.mkdir_p @config.pipes_path
+
+    puts "Generating Configuration to #{@config.config_file}"
+    @config.build
+  end
+
+  desc "Start a Sphinx searchd daemon."
+  task :start => :prepare do
+    Rake::Task['xmlpipes:configure'].invoke unless File.file?(@config.controller.path)
+    raise RuntimeError, "searchd is already running." if @config.controller.running?
+    @config.controller.start
+    if @config.controller.running?
+      puts "Started successfully (pid #{@config.controller.pid})."
+    else
+      puts "Failed to start searchd daemon. Check #{@config.searchd.log}"
+    end
+  end
+
+  desc "Stop Sphinx."
+  task :stop => :prepare do
+    if @config.controller.running?
+      pid = @config.controller.pid
+      @config.controller.stop
+      puts "Stopped search daemon (pid #{pid})."
+    else
+      puts "searchd is not running"
+    end
   end
 
   task :prepare do
-    XMLPipes::Configuration.instance.apply_config
+    @config = XMLPipes::Configuration.instance
+    @config.apply_config
   end
 
 end
